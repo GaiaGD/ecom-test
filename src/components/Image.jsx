1ea168ee2123649d523db293abecdad2ef436556
GaiaGD <gaia.digregorio@gmail.com>
2022-12-02 19:51:37 -0500

    crazy friday

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,19 +1,50 @@
 import React, {useContext, useState} from 'react'
 import { Context } from '../Context'
+import PropTypes from "prop-types"
 
-export default function Image(props){
+function Image({className, image}){
 
+    const {cartItems} = useContext(Context)
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context) 
+    const {toggleFavorite, addToCart} = useContext(Context) 
 
-    const heartIcon = hovered && <i onClick={() => toggleFavorite(props.img.id)} className="ri-heart-line favorite"></i>
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    function heartIcon() {
+        if(image.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(image.id)}></i>
+        } else if(hovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(image.id)}></i>
+        }
+    }
+
+    function cartIcon(itemId){
+        if (cartItems.some(itemInCart => itemInCart.id === itemId)){
+            return <i className="ri-shopping-cart-fill cart"></i>
+        }
+        else if(hovered){
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(image)}></i>
+        }
+    }
 
     return (
-        <div className={`${props.className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
-            {heartIcon}
-            {cartIcon}
-            <img src={props.img.url} className="image-grid" />
+        <div className={`${className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
+            {heartIcon()}
+            {cartIcon(image.id)}
+            <img src={image.url} className="image-grid" />
         </div>
     )
+
+
+    Image.propTypes = {
+
+        className: PropTypes.string,
+
+        image: PropTypes.shape({
+            url: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired,
+            isFavorite: PropTypes.bool
+          })
+    }
+
 }
+
+export default Image
