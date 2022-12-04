27e00dc03a6c54142bec7d78c9615adfa73b5774
GaiaGD <gaia.digregorio@gmail.com>
2022-12-04 16:01:34 -0500

    sunday

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,25 +1,33 @@
 import React, {useState, useContext} from "react"
-import { Context } from '../Context'
+import PropTypes from "prop-types"
+import {Context} from "../Context"
+import useHover from "../hooks/useHover"
 
-function CartItem({item, cost}) {
-    const {removeFromCart} = useContext(Context) 
-    const [hovered, setHovered] = useState(false)
-
-    function cartIcon() {
-        if(hovered) {
-            return <i className="ri-delete-bin-fill" onClick={() => removeFromCart(item.id)}></i>
-        } else {
-            return <i className="ri-delete-bin-line"></i>
-        }
-    }
-
+function CartItem({item}) {
+    const [hovered, ref] = useHover()
+    const {removeFromCart} = useContext(Context)
+    
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+    
     return (
-        <div className="cart-item" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-            {cartIcon()}
+        <div className="cart-item">
+            <i 
+                className={iconClassName} 
+                onClick={() => removeFromCart(item.id)}
+                ref={ref}
+            >
+            </i>
+            
             <img src={item.url} width="130px" />
-            <p id={item.id}>${cost}</p>
+            <p>$5.99</p>
         </div>
     )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
 }
 
 export default CartItem
